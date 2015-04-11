dede990d7f5623613297e8f5928c5ba9644c61e1
Jeremy <yaoxuanw007@gmail.com>
2015-04-10 22:58:45 -0700

    check existing process

--- poc/cli/index.js
+++ poc/cli/index.js
@@ -4,18 +4,31 @@ var fs = require('fs'),
   out = fs.openSync(path.join(__dirname, 'out.log'), 'a'),
   err = fs.openSync(path.join(__dirname, 'out.log'), 'a');
 
+var pidFile = path.join(__dirname, 'pid.json');
+
+try {
+  var data = fs.readFileSync(pidFile, { encoding: 'utf8' }),
+      info = JSON.parse(data);
+  if (info.pid) {
+    console.log('The command (pid: %s) is still running', info.pid);
+    process.exit(0);
+  }
+} catch (e) {
+}
+
 // long run command example
 var child = spawn('node', [ path.join(__dirname, 'bg.js') ], {
   stdio: [ 'ignore', out, err ],
   detached: true
 });
 
-fs.writeFile(path.join(__dirname, 'pid.json'), JSON.stringify({
-    pid: child.pid
-  }), function(err) {
-    child.unref();
-    if(err) {
-      return console.error(err);
-    }
-    console.log("The file was saved!");
+var content = JSON.stringify({ pid: child.pid });
+fs.writeFile(pidFile, content, {
+  encoding: 'utf8'
+}, function (err) {
+  child.unref();
+  if(err) {
+    return console.error(err);
+  }
+  console.log("The file was saved!");
 });
